243f0aa7e1b2577b499896e429b8a20ef16318c2
SuggyCD <105791473+SuggyCD@users.noreply.github.com>
2024-04-08 15:35:44 -0700

    write to file

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -5,38 +5,37 @@ import java.util.Calendar.*
 
 @NonCPS
 def call(Map config=[:]){
-	def dir = new File(pwd());
+        def dir = new File(pwd());
+
+        new File(dir.path + '/releasenotes.txt').withWriter('utf-8')
+        {
+            writer ->
+            dir.eachFileRecurse(FileType.ANY){ file ->
+                if (file.isDirectory()){
+                    writer.writeLine(file.name);
+                }
+                else
+                {
+                    writer.writeLine('\t' + file.name + '\t' + file.length());
+                }
+            }
+        
+
+            def date = new Date();
+            def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+            writer.writeLine("Date and Time IS: " + sdf.format(date));
     
-	new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
-	{ 
-		writer -> 
-	    dir.eachFileRecurse(FileType.ANY){ file ->
-		if (file.isDirectory()){
-		    writer.writeLine(file.name);            
-		}
-		else
-		{
-		    writer.writeLine('\t' + file.name + '\t' + file.length());
-		}
-		} 
-	}
+            writer.writeLine("Build Number is ${BUILD_NUMBER}");
     
-	def date = new Date();
-	def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-	echo "Date and Time IS: " + sdf.format(date)
-
-	echo "Build Number is ${BUILD_NUMBER}";
-
-	def changeLogSets = currentBuild.changeSets;
-	for (change in changeLogSets) {
-		def entries = change.items;
-		for (entry in entries) {
-			echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
-			for (file in entry.affectedFiles) {
-				echo " ${file.editType.name} ${file.path}";				
-			}
-		}
-	}
-
+            def changeLogSets = currentBuild.changeSets;
+            for (change in changeLogSets) {
+                    def entries = change.items;
+                    for (entry in entries) {
+                             writer.writeLine("${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}");
+                            for (file in entry.affectedFiles) {
+                                     writer.writeLine(" ${file.editType.name} ${file.path}";)
+                            }
+                    }
+            }
+        }
 }
-
